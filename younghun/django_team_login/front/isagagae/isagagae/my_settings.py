c411de0f41c8372663b15e46a767530533fe98b7
topyounggod <younghunahn@umass.edu>
2021-11-02 17:12:53 +0900

    22

--- younghun/django_team_login/front/isagagae/isagagae/my_settings.py
+++ younghun/django_team_login/front/isagagae/isagagae/my_settings.py
@@ -3,7 +3,7 @@ DATABASES = {
     'ENGINE':'django.db.backends.mysql',
     'NAME':'TEST', #db이름
     'USER':'root',
-    'PASSWORD':'tldgk132',
+    'PASSWORD':'',
     'HOST':'127.0.0.1',
     'PORT':'3306',
     }
